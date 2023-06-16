3f0ae3590315b5b8ce875fbab74e0be23275a378
Ваше Имя <you@example.com>
2023-06-16 20:44:08 +0300

    скрипт

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+
 if ! command -v docker-compose &> /dev/null
 then
     echo "Docker Compose не установлен, устанавливаем..."
     apt-get update
     apt-get install -y docker-compose
 fi
-    
+
 sudo docker-compose up --build
